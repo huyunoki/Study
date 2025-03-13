3b750cbaa55299177aa0334859a5cdd0aeeb9219
huyunoki <k161616@kansai-u.ac.jp>
2025-03-13 00:45:40 +0000

    Fixed web.php

--- routes/web.php
+++ routes/web.php
@@ -21,26 +21,26 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-// Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(function () {
 
-Route::get('/place', [PlaceController::class, 'index'])->name('dashboard');
+    Route::get('/place', [PlaceController::class, 'index'])->name('dashboard');
 
-Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-Route::get('/places/create', [PlaceController::class, 'create']);
-Route::post('/places/store', [PlaceController::class, 'store']);
-Route::get('/places/{id}', [PlaceController::class, 'show'])->name('markdown.show');
-Route::get('/places/{id}/edit', [PlaceController::class, 'edit']);
-Route::put('/places/{id}/update', [PlaceController::class, 'update']);
-Route::delete('/places/{id}/delete', [PlaceController::class, 'delete']);
+    Route::get('/places/create', [PlaceController::class, 'create']);
+    Route::post('/places/store', [PlaceController::class, 'store']);
+    Route::get('/places/{id}', [PlaceController::class, 'show'])->name('markdown.show');
+    Route::get('/places/{id}/edit', [PlaceController::class, 'edit']);
+    Route::put('/places/{id}/update', [PlaceController::class, 'update']);
+    Route::delete('/places/{id}/delete', [PlaceController::class, 'delete']);
 
-Route::post('/category', [CategoryController::class, 'store'])->name('categories.store');
-Route::get('/category/create', [CategoryController::class, 'create'])->name('category.show');
+    Route::post('/category', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('/category/create', [CategoryController::class, 'create'])->name('category.show');
 
-Route::post('/places/{id}/bookmark', [BookmarkController::class, 'store']);
-Route::delete('/places/{id}/bookmark', [BookmarkController::class, 'destroy']);
-// });
+    Route::post('/places/{id}/bookmark', [BookmarkController::class, 'store']);
+    Route::delete('/places/{id}/bookmark', [BookmarkController::class, 'destroy']);
+});
 
 require __DIR__ . '/auth.php';
