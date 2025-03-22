015e05f99405ab85d470234a25011ead5771ea4c
huyunoki <k161616@kansai-u.ac.jp>
2025-03-22 08:25:06 +0000

    Customize markdown

--- resources/js/markdown.js
+++ resources/js/markdown.js
@@ -1,4 +1,8 @@
 import EasyMDE from "easymde";
 import "easymde/dist/easymde.min.css";
 
-const easyMDE = new EasyMDE({element: document.getElementById('body')});
+const easyMDE = new EasyMDE({
+    element: document.getElementById('body'),
+    sideBySideFullscreen: false,
+    spellChecker: false, 
+});
